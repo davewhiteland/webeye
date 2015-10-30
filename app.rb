d70183c65dee035eea7b1683f539524d15a96127
Dave Whiteland <dave@mysociety.org>
2015-10-30 16:22:50 +0000

    don't listen for POST on different URL: just live on /

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ get '/' do
   erb :index, :locals => {:message => "just a regular webeye... waiting for a webhook"}
 end
 
-post '/webeye' do
+post '/' do
   erb :index, :locals => {:message => "just caught a webhook!"}
 end
 

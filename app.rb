4a9425958038c7f28d9c9b89eedc8266349e55a9
Dave Whiteland <dave@mysociety.org>
2015-11-04 12:23:29 +0000

    anticipate incoming countries.json as the POST payload

--- app.rb
+++ app.rb
@@ -2,12 +2,24 @@ require 'bundler/setup'
 Bundler.require
 Dotenv.load
 
+require 'json'
+
 get '/' do
   erb :index, :locals => {:message => "just a regular webeye... waiting for a webhook"}
 end
 
 post '/' do
-  erb :index, :locals => {:message => "just caught a webhook!"}
+  request.body.rewind
+  payload = request.body.read
+  if payload != nil && payload.to_s.strip.length > 0
+    countries = JSON.parse payload
+    qty_c = countries.length
+    erb :index, :locals => {:message => "Parsed OK: #{qty_c} countries"}
+  else
+    400
+  end
 end
 
-
+error 400 do
+  'Expected countries.json payload in request body'
+end

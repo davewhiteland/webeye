9e24a188b29ddea798322e3bd2291c1e11d12c6a
Dave Whiteland <dave@mysociety.org>
2015-11-05 17:14:26 +0000

    pull down Popolo JSON using URLs constructed from countries.json
    and limit it to one test set (using the Vatican because it's
    a small file for now) while in development
    (took about 3 minutes to pull *all* the legislation JSONs :-) )

--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ Bundler.require
 Dotenv.load
 
 require 'json'
+require 'open-uri'
+
+EP_ROOT_URL = 'http://rawgit.com/everypolitician/everypolitician-data'
 
 get '/' do
   erb :index, :locals => {:message => "just a regular webeye... waiting for a webhook"}
@@ -13,8 +16,31 @@ post '/' do
   payload = request.body.read
   if payload != nil && payload.to_s.strip.length > 0
     countries = JSON.parse payload
+    debug_text = ""
+    all_names = []
+    for country in countries
+
+      #=========================================
+      # FIXME DEBUG: only do the Vatican for now (because: small data set)
+      # FIXME DEBUG: otherwise this is a lot of activity every time it's run
+      if ! /Vatican/.match(country['name'])
+        next
+      end
+      #=========================================
+
+      for legislature in country['legislatures']
+        sha = legislature['sha']
+        # has the SHA changed since "last time"? TODO
+        url = "#{EP_ROOT_URL}/#{sha}/#{legislature['popolo']}"
+        popolo_json = open(url).read
+        popolo = JSON.parse(popolo_json)
+        for person in popolo['persons']
+          debug_text += "#{country['name']}: #{person['id']}, #{person['name']}\n"
+        end
+      end
+    end
     qty_c = countries.length
-    erb :index, :locals => {:message => "Parsed OK: #{qty_c} countries"}
+    erb :index, :locals => {:message => "Parsed OK: #{qty_c} countries", :debug_text => debug_text}
   else
     400
   end
